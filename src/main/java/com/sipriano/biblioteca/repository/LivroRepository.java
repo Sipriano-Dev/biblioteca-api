09b1e1afd24348a8f0a9377dda10932da161ccbc
Anderson Sipriano <adsspns@gmail.com>
2026-01-09 03:22:18 -0300

    chore: cria estrutura de camadas da aplicação

---
+++ src/main/java/com/sipriano/biblioteca/repository/LivroRepository.java
@@ -0,0 +1,7 @@
+package com.sipriano.biblioteca.repository;
+
+import com.sipriano.biblioteca.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Long, Livro> {
+}
