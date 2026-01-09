3c6658933ae96e45215fe8e1cfb8b1ae06aac5f0
Anderson Sipriano <adsspns@gmail.com>
2026-01-09 03:14:44 -0300

    feat: adiciona entidade Livro

---
+++ src/main/java/com/sipriano/biblioteca/domain/Livro.java
@@ -0,0 +1,32 @@
+package com.sipriano.biblioteca.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String autor;
+
+    @Column(nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Column(nullable = false)
+    private String isbn;
+
+}
