9b920a6efd2b1aae9d9a95ae567f213cdba379e5
Anderson Sipriano <adsspns@gmail.com>
2026-04-08 13:04:50 -0300

    melhora filtro de busca por nome

--- src/main/java/com/sipriano/biblioteca/repository/AutorRepository.java
+++ src/main/java/com/sipriano/biblioteca/repository/AutorRepository.java
@@ -2,11 +2,14 @@ package com.sipriano.biblioteca.repository;
 
 import com.sipriano.biblioteca.domain.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
 
-    List<Autor> findByNomeIgnoreCase(String nome);
+    @Query("select a from Autor a where a.nome like %:nome%")
+    List<Autor> findByNomeIgnoreCase(@Param("nome") String nome);
 
 }
